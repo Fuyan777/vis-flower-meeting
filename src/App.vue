d870b5cde0a3c95cfdf28aba71a1e8abced88aec
Fuyan777 <fuyamiwa713@gmail.com>
2022-10-30 00:37:05 +0900

    Add nod radian

--- src/App.vue
+++ src/App.vue
@@ -37,11 +37,12 @@
       </div>
     </div>
     <div class="debug-setting-block">
-      <h2>■ デバッグ</h2>
+      <h2>■ デバッグ: {{ isDebug }}</h2>
       <div class="count-all">
         <li>発話回数   ：{{ speechCount }}</li>
         <li>頷き回数   ：{{ nodCount }}</li>
         <li>予備動作回数：{{ motivationCount }}</li>
+        <button class="test-all-debug-button" v-on:click="startDebug">Debug Start</button>
         <button class="test-all-reset-button" v-on:click="resetCountPlayer">Reset Count</button>
       </div>
       <div class="speech" v-bind="speechCount">
@@ -62,6 +63,7 @@
 <script>
 import vad from 'voice-activity-detection';
 import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection';
+import * as tf from '@tensorflow/tfjs';
 import positionArray from "./position.js";
 import firebaseConfig from "./firebase.js";
 import { initializeApp } from "firebase/app";
@@ -82,7 +84,7 @@ export default {
       faceModel: null,
       video: null,
       detector: null,
-      meanPose: [],
+      meanNodArray: [],
       nodAverageScore: 0,
       nodCount: 0,
       motivationCount: 0,
@@ -131,7 +133,8 @@ export default {
         motivation: 0,
         nod: 0,
         speech: 0
-      }
+      },
+      isDebug: false,
     }
   },
   mounted: function () {
@@ -153,23 +156,27 @@ export default {
   },
   watch: {
     nodCount: function() {
+      if (this.isDebug) return; 
+      console.log("post");
       this.postCountPlayer();
     },
     speechCount: function() {
+      if (this.isDebug) return; 
       this.postCountPlayer();
     },
     motivationCount: function() {
+      if (this.isDebug) return; 
       this.postCountPlayer();
     },
   },
   computed: {
     calcSum: function() {
-      return this.meanPose.reduce(function(sum, element) {
+      return this.meanNodArray.reduce(function(sum, element) {
         return sum + element;
       })
     },
     calcAverage: function() {
-      return this.calcSum / this.meanPose.length;
+      return Math.floor(this.calcSum / this.meanNodArray.length);
     }
   },
   methods: {
@@ -277,6 +284,9 @@ export default {
       this.stopTracking();
       this.stopVAD();
     },
+    startDebug: function() {
+      this.isDebug = !this.isDebug;
+    },
     setFlowerCountWithDB: function(playerID, docData) {
       const flowerCount = docData.speech + docData.nod;
       for (let i=0; i<flowerCount; i++) {
@@ -364,27 +374,36 @@ export default {
         const faces = await this.detector.estimateFaces(this.video);
 
         // MARK: - Nod Recognition
-        this.meanPose.push(faces[0].keypoints[1].y);
-        // keypoint
-        this.outputDebug("nod point",faces[0].keypoints[1].y);
 
-        // meanPoseのサンプル数が10以下の場合、skipする
-        if (this.meanPose.length < 10) {
+        // 顔の角度計算
+        const rad = this.calcFaceRotation(faces);
+        console.log("rad_x: ",rad[0],"rad_y: ",rad[1]);
+        this.meanNodArray.push(rad[1]);
+
+        console.log("meanNodArray: ", this.meanNodArray);
+
+        // keypoint
+        this.outputDebug("nod point", rad[1]);
+
+        // meanNodArrayのサンプル数が10以下の場合、skipする
+        if (this.meanNodArray.length < 10) {
           this.nodCount = 0;
           this.nodAverageScore = this.calcAverage;
           console.log("nod count: skip");
-          return
         }
-
-        // 10秒ごとのnodの平均算出
-        this.outputDebug("nod average", this.nodAverageScore);
-        // 平均の基準よりも下を向いているか
-        if (this.nodAverageScore + 30 < faces[0].keypoints[1].y) {
-          console.log("****** nod count ******");
-          this.nodCount += 1;
-          this.countStatus.nod += 1;
-          console.log(this.nodCount);
-          this.outputDebug("this.countStatus.nod", this.countStatus.nod);
+        
+        if (this.meanNodArray.length >= 10) {
+          // 10秒ごとのnodの平均算出
+          this.outputDebug("nod average", this.nodAverageScore);
+          // 平均の基準よりも下を向いているか
+          if (this.nodAverageScore-5 > rad[1]) {
+            console.log("****** nod count ******");
+            this.nodCount += 1;
+            this.countStatus.nod += 1;
+            console.log(this.nodCount);
+            this.outputDebug("this.countStatus.nod", this.countStatus.nod);
+          }
+          this.meanNodArray.shift();
         }
 
         // MARK: - Mouth Recognition
@@ -454,6 +473,81 @@ export default {
       if(value === null) { value = "" }
 
       console.log("【Debug】: "+message+" - "+value);
+    },
+    calcFaceRotation: function(faces) {
+      let faceOvalArray = [];
+
+      faces[0].keypoints.forEach((keypoint) => {
+        if (keypoint.name == undefined) return
+        if (keypoint.name == 'faceOval') {
+          faceOvalArray.push([keypoint.x, keypoint.y, keypoint.z]);
+        }
+      });
+
+      // tfの配列格納
+      let faceMulciArray = tf.tensor(faceOvalArray);
+
+      // 目の算出
+      const rightEye = tf.tensor1d([
+        faces[0].keypoints[33].x, faces[0].keypoints[33].y, faces[0].keypoints[33].z
+      ]);
+      const leftEye = tf.tensor1d([
+        faces[0].keypoints[263].x, faces[0].keypoints[263].y, faces[0].keypoints[263].z
+      ]);
+
+      // 顔点群の正規化
+      const faceNorm = faceMulciArray.div(tf.norm(rightEye.sub(leftEye)).mul(0.06));
+      const centerPosition = faceNorm.sub(faceNorm.mean(0));
+
+      const c00 = centerPosition.slice(0, 1).as1D();
+      const c09 = centerPosition.slice(9, 1).as1D();
+      const c18 = centerPosition.slice(18, 1).as1D();
+      const c27 = centerPosition.slice(27, 1).as1D();
+
+      // 回転行列
+      const rotate0 = c18.sub(c00).div(tf.norm(c18.sub(c00))); // 顔の垂直方向
+      const rotate1 = c09.sub(c27).div(tf.norm(c09.sub(c27))); // 顔の水平方向
+
+      // tfに変換
+      let rotate = tf.concat([rotate0, rotate1]).arraySync();
+
+      // y座標のベクトル
+      const m00 = rotate[0];
+      const m01 = rotate[1];
+      const m02 = rotate[2];
+
+      // x座標のベクトル
+      const m10 = rotate[3];
+      const m11 = rotate[4];
+      const m12 = rotate[5];
+
+      // 外積より法線ベクトルを求める
+      const m20 = m01 * m12 - m02 * m11;
+      const m21 = m02 * m10 - m00 * m12;
+      const m22 = m00 * m11 - m01 * m10;
+
+      const sy = Math.sqrt(m00 * m00 + m10 * m10);
+      const X = (Math.atan2(m21, m22) * 180) / Math.PI;
+      const Y = (Math.atan2(-m20, sy) * 180) / Math.PI;
+      const Z = (Math.atan2(m10, m00) * 180) / Math.PI;
+
+      let radX = 0;
+      let radY = 0;
+
+      //Y軸のマイナス値の変更
+      if (X < 0) {
+        radY = Math.floor(X) + 360;
+      } else {
+        radY = X;
+      }
+
+      if (radY < 100) {
+        radY += 300;
+      }
+
+      radX = Math.floor(Y) + 45;
+
+      return [radX, radY, Z];
     }
   }
 }
